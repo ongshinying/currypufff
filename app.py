5f385b7929149edfc0061d8c89768b1417eaa097
ongshinying <ongshinying28@gmail.com>
2024-10-08 12:35:29 +0000

    1

--- app.py
+++ app.py
@@ -4,60 +4,19 @@ import os
 import numpy as np
 import textblob
 
-model = genai.GenerativeModel("gemini-1.5-flash")
-genai.configure(api_key="AIzaSyDJIFt85afmdExwzpO9V2RjRis27t7muEk")
-
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
+ 
+@app.route("/subscription",methods=["GET","POST"])
+def subscription():
+    return(render_template("subscription.html"))
 
-@app.route("/prediction_DBS",methods=["GET","POST"])
-def prediction_DBS():
-    return(render_template("prediction_DBS.html"))
+@app.route("/thankyou",methods=["GET","POST"])
+def thankyou():
+    return(render_template("thankyou.html",r=r))
 
-@app.route("/prediction_result_DBS",methods=["GET","POST"])
-def prediction_result_DBS():
-    q = float(request.form.get("q"))
-    r = (-50.6 * q )+ 90.2
-    return(render_template("prediction_result_DBS.html",r=r))
-   
-@app.route("/faq",methods=["GET","POST"])
-def faq():
-    return(render_template("faq.html"))
-
-@app.route("/q1",methods=["GET","POST"])
-def q1():
-    r = model.generate_content("How should I diversify my investment portfolio?")
-    return(render_template("q1_reply.html",r=r))
-
-@app.route("/q2",methods=["GET","POST"])
-def q2():
-    q = request.form.get("q")
-    r = model.generate_content(q)
-    return(render_template("q2_reply.html",r=r))
-
-@app.route("/prediction_creditability",methods=["GET","POST"])
-def prediction_creditability():
-    return(render_template("prediction_creditability.html"))
-
-@app.route("/prediction_result_creditability",methods=["GET","POST"])
-def prediction_result_creditability():
-    q = float(request.form.get("q"))
-    r = (-9.34111523e-05 * q )+ 1.15201551
-    r = np.where(r>=0.5, "Creditable", "Not Creditable")
-    return(render_template("prediction_result_creditability.html", r=r))
-
-@app.route("/sentiment_analysis",methods=["GET","POST"])
-def sentiment_analysis():
-    return(render_template("sentiment_analysis.html"))
-
-@app.route("/sentiment_analysis_result",methods=["GET","POST"])
-def sentiment_analysis_result():
-    q = request.form.get("q")
-    r = textblob.TextBlob(q).sentiment
-    return(render_template("sentiment_analysis_result.html",r=r))
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run()
